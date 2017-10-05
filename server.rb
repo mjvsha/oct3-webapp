caba2b0697e66721e2342f5e61bad59b4e119fbf
Meera <mjvsharma@outlook.com>
2017-10-05 11:01:43 -0400

    wrote my first web app but i need to fix the links

--- server.rb
+++ server.rb
@@ -7,3 +7,29 @@ end
 get '/portfolio' do
   erb :gallery
 end
+
+get '/about_me' do
+  @skills= ['git', 'github', 'html', 'css', 'ruby', 'sql','javascript']
+  @interests = ['reading', 'writing', 'learning', 'dancing', 'working out', 'hiking', 'adventure', 'programming']
+  erb :about_me
+
+end
+
+get '/favorites' do
+
+@fav_links = ["http://radio.garden/", "https://www.google.ca/maps", "http://cogsys.ubc.ca/", "https://www.utoronto.ca/", "http://www.hillarycoe.com/" ]
+
+erb :favorites
+end
+
+
+get '/' do
+
+redirect to('/home')
+
+end
+
+get '/gallery' do
+  redirect to('/portfolio')
+
+end
